49dc902bd9c1fdbab0822f55b3d8734b04f77af8
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-08-07 14:01:48 +0200

    added more functionality to the cunter

--- src/app/features/counter/Counter.js
+++ src/app/features/counter/Counter.js
@@ -1,9 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./counterSlice";
+import { decrement, increment, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
 
   return (
     <section>
@@ -12,8 +21,17 @@ const Counter = () => {
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
       </div>
+      <input
+        type='text'
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+        <button onClick={() => resetAll()}>Rest</button>
+      </div>
     </section>
   );
 };
 
-export default Counter
+export default Counter;
