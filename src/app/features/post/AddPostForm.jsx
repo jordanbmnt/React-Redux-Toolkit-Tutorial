e3ae2439657d698a26cefa8b0a394e9bdd77b9b4
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-08-07 16:01:14 +0200

    improved reducer

--- src/app/features/post/AddPostForm.jsx
+++ src/app/features/post/AddPostForm.jsx
@@ -15,11 +15,7 @@ const AddPostForm = () => {
   const onSavePostClicked = () => {
     if (title && content) {
       dispatch(
-        postAdded({
-          id: nanoid(),
-          title,
-          content,
-        })
+        postAdded(title, content)
       );
 
       setTitle("");
