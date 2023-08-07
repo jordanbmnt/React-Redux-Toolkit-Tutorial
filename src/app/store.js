f4f3772e1de5888815824eb0a1c566a4eed50f6a
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-08-07 14:51:58 +0200

    created slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postReducer from "./features/post/postSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postReducer,
+  },
 });
