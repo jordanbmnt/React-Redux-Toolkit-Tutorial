ac8d6f56c47e7457032820b4c4649e487efc3439
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-08-07 13:51:01 +0200

    added counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
+import Counter from "./app/features/counter/Counter";
 
 function App() {
   return (
-    <div className="App">
-      
-    
+    <div className='App'>
+      <Counter />
     </div>
   );
 }
